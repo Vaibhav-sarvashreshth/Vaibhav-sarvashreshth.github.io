6a494041dcc1a2be4e9224f57d5893dd7cbb1256
Vaibhav Mokale <mokalevaibhav@gmail.com>
2023-06-10 13:03:05 +0530

    Comment

---
+++ docs/deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "Starting to update the repo......"
+
+git status
+git add .
+git commit -m "Comment"
+git push
+
+
+echo "Repo updation completed succesfully ......"
+echo "You can see your website here : https://vaibhav-sarvashreshth.github.io/"
